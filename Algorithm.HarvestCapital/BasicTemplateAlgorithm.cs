356e783bd249781e9ade418c1f2108d9b690c563
Mike <mike@xsdconsulting.com>
2016-02-06 18:58:58 -0500

    Update

--- Algorithm.HarvestCapital/BasicTemplateAlgorithm.cs
+++ Algorithm.HarvestCapital/BasicTemplateAlgorithm.cs
@@ -1,19 +1,6 @@
-﻿/*
- * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
- * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-*/
-
-using QuantConnect.Data;
+﻿using QuantConnect.Data;
+using QuantConnect.Data.Market;
+using System;
 
 namespace QuantConnect.Algorithm.HarvestCapital
 {
@@ -27,11 +14,17 @@ namespace QuantConnect.Algorithm.HarvestCapital
         /// </summary>
         public override void Initialize()
         {
-            SetStartDate(2013, 10, 07);  //Set Start Date
-            SetEndDate(2013, 10, 11);    //Set End Date
-            SetCash(100000);             //Set Strategy Cash
+            SetStartDate(2015, 7, 1);   //Set Start Date
+            SetEndDate(2015, 7, 15);    //Set End Date
+            SetCash(100000);            //Set Strategy Cash
+                       
             // Find more symbols here: http://quantconnect.com/data
-            AddSecurity(SecurityType.Equity, "SPY", Resolution.Second);
+            AddSecurity(SecurityType.Equity, "SPY", Resolution.Daily);
+
+            //var s = Securities["SPY"];
+            //var s = Portfolio.Securities["SPY"];
+            //Transactions
+            
         }
 
         /// <summary>
@@ -39,12 +32,46 @@ namespace QuantConnect.Algorithm.HarvestCapital
         /// </summary>
         /// <param name="data">Slice object keyed by symbol containing the stock data</param>
         public override void OnData(Slice data)
-        {
+        {            
+            var SPY = data["SPY"];
+
+            Plot("TEST", SPY.Close);
+
+            var orders = this.Transactions.GetOrders(x => x.Symbol == SPY.Symbol);
+
             if (!Portfolio.Invested)
             {
-                SetHoldings("SPY", 1);
-                Debug("Purchased Stock");
+                SetHoldings("SPY", .5);
+                Debug("Purchased Stock");                
             }
+            else
+            {
+                MarketOrder("SPY", 10);
+                Debug("Purchased Stock");
+            }            
         }
+
+        // Second / Minute / Hour / Daily level data
+        public void OnData(TradeBars data)
+        {
+
+        }
+
+        // Tick level data
+        // When Resolution.Tick this is called every second with an array of tick values for that second
+        public void OnData(Ticks data)
+        {
+
+        }
+
+        public override void OnEndOfDay()
+        {
+            base.OnEndOfDay();
+        }
+
+        public override void OnEndOfAlgorithm()
+        {            
+            base.OnEndOfAlgorithm();
+        }       
     }
 }
